d9a1a36b5fa9d65fe92edf2478d0be8f33738c98
Noah  Klem <klem.noah@gmail.com>
2021-05-28 23:58:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(array)
+  case array.count
+  when 0
+    puts "The line is currently empty."
+  else
+    
+    array.map do |name, index|
+       puts "#{index+1}. #{name}"
+    end
+  end
+end

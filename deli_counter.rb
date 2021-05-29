a34d56d874dd4dc28f264da9fe40658aea53a883
Noah  Klem <klem.noah@gmail.com>
2021-05-29 00:00:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
   when 0
     puts "The line is currently empty."
   else
-    
+    begin_string = "The "
     array.map do |name, index|
        puts "#{index+1}. #{name}"
     end

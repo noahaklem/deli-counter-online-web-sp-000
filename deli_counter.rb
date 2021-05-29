4d42bde074b1b133fdd64873f71df6c909fe6fda
Noah  Klem <klem.noah@gmail.com>
2021-05-29 00:26:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,6 @@ end
 
 def take_a_number(array, name)
   array << name
-  "Welcome, #{name}. You are number #{array.length} in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
   

fcbf9ac4c38cdd1f3aa973a68856b517a71db414
Noah  Klem <klem.noah@gmail.com>
2021-05-29 00:09:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(array)
   when 0
     puts "The line is currently empty."
   else
-    begin_string = "The line is currently: "
+    begin_string = "The line is currently:"
     array.each.with_index() do |name, index|
        begin_string<<" #{index+1}. #{name}"
     end
